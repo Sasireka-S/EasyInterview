b2722e2c169056a177113403a5ad118a59a99fe7
Sasireka-S <74347610+Sasireka-S@users.noreply.github.com>
2022-12-11 22:10:25 +0530

    Update 2Company Sign In.py

--- pages/2Company Sign In.py
+++ pages/2Company Sign In.py
@@ -67,7 +67,7 @@ with txt.form(key='form3'):
     st.markdown(original_title, unsafe_allow_html=True)
     id = text_field("Organization ID : ")
     password = text_field("Password : ", type = "password")
-    st.text("Start meeting for Recruitment [link](videochat2.html)")
+    st.write("Start meeting for Recruitment [link](videochat2.html)")
     submit = st.form_submit_button(label="Submit")
 if submit:
     txt.empty()
